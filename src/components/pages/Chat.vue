3dd4fd96045efe91ca68c1af7c729392f344d997
TakanoriShima <quark2galaxy@gmail.com>
2022-10-06 18:08:26 +0900

    チャット画面を実装しよう

---
+++ src/components/pages/Chat.vue
@@ -0,0 +1,33 @@
+<!-- <script setup>でない点に注意 -->
+<script>
+import { defineComponent, reactive } from 'vue'
+import View from '../chat/View.vue' // 追加
+
+// TypeScript に Vue コンポーネントオブションのなかで適切に型を推論させるために、defineComponent グローバルメソッドでコンポーネントを定義する必要があります:
+export default defineComponent({
+  components: {
+    View
+  },
+  setup() {
+    const data = reactive({
+      user: {},
+      chat: [],
+      input: '',
+      displayName: ''
+    })
+    return {
+      data
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="container">
+    <View />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import * as vueRouter from "vue-router";
 import Login from "../components/pages/Login.vue";
 import SignUp from "../components/pages/SignUp.vue";
+import Chat from "../components/pages/Chat.vue"; // 追加
 
 const routes = [
   {
     path: "/",
-    component: Login,
+    component: Chat, // LoginからChatに変更
   },
   {
     path: "/signup",
     component: SignUp,
+  },
+  {
+    path: "/login", // 追加
+    component: Login, // 追加
   },
 ];
 
